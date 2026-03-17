5e54ffcbed2d27eb6f7a326fed24fa5b6bb09264
William Kearney <william.kearney@uni-potsdam.de>
2026-03-17 09:16:15 +0100

    Add a buildtool task to build the MATLAB notebooks (#8)
    The task installs TopoToolbox from the latest release on GitHub if it
    is not already installed and then runs the (single) notebook and
    outputs it as a Jupyter notebook. Sphinx should then be able to find
    this notebook and render it.
    
    The notebook is converted from an M file to an MLX file. According to
    MathWorks, the export to LaTeX/Jupyter works properly with MLX files,
    but not with M files. This is apparently being worked on, and I would
    like to eventually switch back to M files because of their better
    integration with source control.

---
+++ buildfile.m
@@ -0,0 +1,51 @@
+function plan = buildfile
+
+% Create a plan from task functions
+plan = buildplan(localfunctions);
+
+% Make the "archive" task the default task in the plan
+plan.DefaultTasks = "docs";
+
+plan("docs").Dependencies = "installtt3";
+end
+
+function installtt3Task(~)
+
+tlbxs = struct2table(matlab.addons.toolbox.installedToolboxes());
+
+if ismember("TopoToolbox", tlbxs.Name)
+    return
+end
+
+% Github information
+owner = "topotoolbox";
+repo  = "topotoolbox3";
+url   = sprintf("https://api.github.com/repos/%s/%s/releases/latest", ...
+                owner, repo);
+
+% Read API to get the latest 
+opts = weboptions("ContentType","json","Timeout",10);
+json  = webread(url,opts);
+
+archstr = computer('arch');
+
+assets  = json.assets;
+I = contains({assets.name},archstr);
+
+directory   = tempdir;
+toolboxfile = fullfile(directory,assets(I).name);
+file = websave(toolboxfile,...
+    assets(I).browser_download_url);
+
+matlab.addons.toolbox.installToolbox(file);
+
+delete(toolboxfile)
+
+end
+
+function docsTask(~)
+
+export("notebooks/matlab/introduction.mlx", ...
+    "notebooks/matlab/introduction.ipynb", Run=true)
+
+end
