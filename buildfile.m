f0d230fcb27e9d8014100a8ab93804973b1c6ebb
William Kearney <william.kearney@uni-potsdam.de>
2026-03-19 10:12:22 +0100

    Recode MATLAB generated images so they get picked up as thumbnails (#21)
    This PR adds a jq postprocessing step of the generated notebooks. This
    extracts the embedded image data and replaces it with a different
    mimetype (image/png) so that it gets recognized as an image by
    nbsphinx.
    
    jq is called with the `system` function. jq is installed by default on
    the CI machines that we build the gallery on but may not be installed
    on all developer machines.
    
    This solution is somewhat fragile as it depends on MATLAB not changing
    its Jupyter notebook output format too much.
    
    Signed-off-by: William Kearney <william.kearney@uni-potsdam.de>

--- buildfile.m
+++ buildfile.m
@@ -55,7 +55,26 @@ for i = 1:size(notebooks, 1)
     nb = fullfile(path, name+".ipynb");
 
     fprintf("Rendering %s as %s\n", mlx, nb)
-    export(mlx, nb, Run=true);
+    export(mlx, nb, Run=true, FigureFormat="png");
+
+    % MATLAB currently (R2025b) exports embedded images in output cells
+    % with the text/html mimetype. The images are embedded in <img> tags
+    % with a data URL. This jq (https://jqlang.org/) filter extracts the
+    % data from this URL and replaces the "text/html" object in the output
+    % data with an "image/png" one. This works, but depends on how MATLAB
+    % chooses to export Jupyter notebooks. Bad things will also happen if
+    % any other output cells have the text/html mimetype.
+    jqfilter = ['''.cells[].outputs[]?.data |= if has("text/html") then ' ...
+        '{"image/png" : [."text/html"[] | ' ...
+        'capture("data:image/png;base64,(?<x>[A-Za-z0-9/=+]*)\"").x]} ' ...
+        'else . end'' '];
+    % jq will by default print colored output, which gets stored as ANSI
+    % escape codes. The -M option turns this off.
+    [status, cout] = system(['jq -M ' jqfilter char(nb)]);
+    if status ~= 0
+        error("jq unable to recode images: [%s] %s", status, cout);
+    end
+    writelines(cout, nb);
 end
 
 end
